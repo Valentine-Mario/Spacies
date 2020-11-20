e7a8a3713d1e6ac07351813fe02da167b42136da
valentine-mario <valentine13400@gmail.com>
2020-11-20 13:59:51 +0100

    added user spaces schema

---
+++ server/migrations/2020-11-20-124024_add_space/up.sql
@@ -0,0 +1,18 @@
+-- Your SQL goes here
+CREATE TABLE spaces (
+  id SERIAL NOT NULL PRIMARY KEY,
+  spaces_name TEXT NOT NULL UNIQUE,
+  spaces_desc TEXT NOT NULL,
+  spaces_img TEXT NOT NULL DEFAULT 'https://res.cloudinary.com/rchain/image/upload/v1601545130/default-logo.png',
+  created_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE spaces_users(
+    id SERIAL NOT NULL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    spaces_id INTEGER NOT NULL,
+    admin_status BOOLEAN NOT NULL,
+
+    foreign key (user_id) REFERENCES users(id),
+    foreign key (spaces_id) REFERENCES spaces(id)
+);
